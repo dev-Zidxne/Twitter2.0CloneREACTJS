375979c1a7aa9872a0da39ea06ff186f5c3e9953
Zidane Innis <zidaneinnis@gmail.com>
2022-07-20 17:28:01 -0500

    Twitter 2.0 Clone

---
+++ components/Sidebar.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import {
+  BellIcon,
+  HashtagIcon,
+  BookmarkIcon,
+  CollectionIcon,
+  DotsCircleHorizontalIcon,
+  MailIcon,
+  UserIcon,
+  HomeIcon,
+} from "@heroicons/react/outline";
+
+function Sidebar() {
+  return (
+    <div>
+      <img className="h-10 w-10" src="http://links.papareact.com/drq"></img>
+    </div>
+  );
+}
+
+export default Sidebar;
---
+++ components/SidebarRow.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface Props {
+  Icon: () => {
+    title: string;
+  };
+}
+
+function SidebarRow(props: Props) {
+  return <div>SidebarRow</div>;
+}
+
+export default SidebarRow;
