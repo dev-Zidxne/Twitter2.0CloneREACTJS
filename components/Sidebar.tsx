718d8327df66a6c8a7500e8036564de4d77c0478
Zidane Innis <zidaneinnis@gmail.com>
2022-07-20 18:22:51 -0500

    Implemented Twitter Feed Embed
    Added Search Section

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -9,11 +9,21 @@ import {
   UserIcon,
   HomeIcon,
 } from "@heroicons/react/outline";
+import SidebarRow from "./SidebarRow";
 
 function Sidebar() {
   return (
-    <div>
-      <img className="h-10 w-10" src="http://links.papareact.com/drq"></img>
+    <div className="flex flex-col">
+      <img className="h-10 w-10" src="http://links.papareact.com/drq" alt="" />
+      <SidebarRow Icon={HomeIcon} title="Home" />
+      <SidebarRow Icon={HashtagIcon} title="Explore" />
+      <SidebarRow Icon={BellIcon} title="Notifications" />
+      <SidebarRow Icon={MailIcon} title="Messages" />
+      <SidebarRow Icon={BookmarkIcon} title="Bookmarks" />
+
+      <SidebarRow Icon={CollectionIcon} title="Lists" />
+      <SidebarRow Icon={UserIcon} title="Sign-In" />
+      <SidebarRow Icon={DotsCircleHorizontalIcon} title="More" />
     </div>
   );
 }
--- components/SidebarRow.tsx
+++ components/SidebarRow.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 
 interface Props {
-  Icon: () => {
-    title: string;
-  };
+  Icon: (props: React.ComponentProps<"svg">) => JSX.Element;
+  title: string;
 }
 
-function SidebarRow(props: Props) {
-  return <div>SidebarRow</div>;
+function SidebarRow({ Icon, title }: Props) {
+  return (
+    <div className="group flex max-w-fit cursor-pointer items-center space-x-2 rounded-full px-4 py-3  transition-all duration-200 hover:bg-gray-100">
+      <Icon className="h-6 w-6" />
+      <p className="group-hover:text-twitter">{title}</p>
+    </div>
+  );
 }
 
 export default SidebarRow;
