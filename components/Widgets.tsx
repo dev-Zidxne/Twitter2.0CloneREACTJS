1d49d55cb521d03bb95e666bfe956bc06e4bbc8c
Zidane Innis <zidaneinnis@gmail.com>
2022-07-22 09:32:30 -0500

    Added Feed mid-section And finalized widgets

--- components/Widgets.tsx
+++ components/Widgets.tsx
@@ -4,7 +4,7 @@ import { TwitterTimelineEmbed } from "react-twitter-embed";
 
 function Widgets() {
   return (
-    <div className="mt-2 px-2">
+    <div className="mt-2  col-span-2 hidden px-2 lg:inline">
       {/*Search */}
       <div className="mt-2 flex item-center space-x-2 bg-grat-100 p-3 rounded-full">
         <SearchIcon className="h-5 w-5 text-gray-400" />
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,12 +7,12 @@ import Widgets from "../components/Widgets";
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <div className="lg:max-w-6xl max-h-screen overflow-hidden">
       <Head>
         <title>Twitter 2.0</title>
       </Head>
 
-      <main>
+      <main className="grid grid-cols-9">
         <Sidebar />
 
         <Feed />
